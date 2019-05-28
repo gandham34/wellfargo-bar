9e3793de9b8eb690cab41bc92a2e90f7f05c7250
gandham34 <51077176+gandham34@users.noreply.github.com>
2019-05-28 09:01:21 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,11 +2,11 @@
 
 pwd
 ls -ltr
-touch test
+touch Test
 ls -ltr
-echo "testing" >> test
+echo "testing" >> Test
 ls -ltr
-echo "$Env for $Variable" >> test
+echo "$Env for $Variable" >> Test
 echo "[INFO] GIT Jenkins Integratioon Demo"
 echo "[INFO] POLL SCM Demo"
 echo "[INFO] BRANCH Demo"
